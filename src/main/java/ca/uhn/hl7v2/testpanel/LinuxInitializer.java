3e3b11b2797c9b4d7f430ca805a6fd0830c91fad
nicola <nicoladenisco@gmail.com>
2024-02-06 16:47:46 +0100

    mia versione

---
+++ src/main/java/ca/uhn/hl7v2/testpanel/LinuxInitializer.java
@@ -0,0 +1,22 @@
+package ca.uhn.hl7v2.testpanel;
+
+import ca.uhn.hl7v2.testpanel.controller.Controller;
+import javax.swing.UIManager;
+
+public class LinuxInitializer
+{
+
+  public void run(final Controller theController)
+  {
+    try
+    {
+      // Set System L&F
+      UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+//			UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+    }
+    catch(Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
+}
